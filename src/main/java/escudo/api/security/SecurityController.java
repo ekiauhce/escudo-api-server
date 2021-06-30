ca00bdec0ce40c94f848739a111f3fab2a29bcc7
ekiauhce <ekiauhce@yandex.ru>
2021-06-30 20:13:58 +0300

    Add login endpoint

--- src/main/java/escudo/api/security/SecurityController.java
+++ src/main/java/escudo/api/security/SecurityController.java
@@ -2,38 +2,45 @@ package escudo.api.security;
 
 import escudo.api.Buyer;
 import escudo.api.BuyerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("register")
-public class RegisterController {
+public class SecurityController {
 
     private final BuyerRepository repo;
     private final PasswordEncoder encoder;
 
-    @Autowired
-    public RegisterController(BuyerRepository repo, PasswordEncoder encoder) {
+    public SecurityController(BuyerRepository repo, PasswordEncoder encoder) {
         this.repo = repo;
         this.encoder = encoder;
     }
 
-    @PostMapping
-    public ResponseEntity<Void> register(@RequestBody Credentials credentials) {
+    @PostMapping("register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Credentials register(@RequestBody Credentials credentials) {
         try {
             Buyer buyer = new Buyer(credentials.getUsername(), encoder.encode(credentials.getPassword()));
             repo.save(buyer);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            return credentials;
         } catch (DataIntegrityViolationException e) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "User with this username already exists!", e);
         }
+    }
+
+    @GetMapping("login")
+    @ResponseStatus(HttpStatus.OK)
+    public Credentials login(@RequestBody Credentials credentials) {
+        Buyer buyer = repo.findByUsername(credentials.getUsername());
+        if (buyer == null || credentials.getPassword() == null ||
+                !encoder.matches(credentials.getPassword(), buyer.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        return credentials;
     }
 }
