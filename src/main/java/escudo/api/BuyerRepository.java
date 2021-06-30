f5bcab83e629c14d40d79876c6e508295c3fd559
ekiauhce <ekiauhce@yandex.ru>
2021-06-30 20:10:24 +0300

    Remove unused method

--- src/main/java/escudo/api/BuyerRepository.java
+++ src/main/java/escudo/api/BuyerRepository.java
@@ -2,11 +2,7 @@ package escudo.api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface BuyerRepository extends JpaRepository<Buyer, Long> {
 
     Buyer findByUsername(String username);
-
-    Boolean existsByUsername(String username);
 }
