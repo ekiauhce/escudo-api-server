06d88814e3bc4f4441f00a6b8ebefc8dbf6e65ad
ekiauhce <ekiauhce@yandex.ru>
2021-06-30 20:13:10 +0300

    Delete boughtAt field and add unique constraint

--- src/main/java/escudo/api/Product.java
+++ src/main/java/escudo/api/Product.java
@@ -1,34 +1,34 @@
 package escudo.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
-@Table(name = "products")
+@Table(name = "products",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"name", "buyer_id"})})
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
     private String name;
-
-    private Date boughtAt;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "buyer_id", referencedColumnName = "id")
     @JsonIgnore
     private Buyer buyer;
 
-    @PrePersist
-    public void boughtAt() {
-        boughtAt = new Date();
-    }
-
-
+    @OneToMany(mappedBy = "product", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    private List<Purchase> purchases = new ArrayList<>();
 }
