4b89d95c808bd2080a8f0bf2114ad11844f7efef
ekiauhce <ekiauhce@yandex.ru>
2021-06-30 20:15:11 +0300

    Create purchase controller

---
+++ src/main/java/escudo/api/PurchaseController.java
@@ -0,0 +1,31 @@
+package escudo.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+public class PurchaseController {
+
+    private final EscudoService escudoService;
+
+    public PurchaseController(EscudoService escudoService) {
+        this.escudoService = escudoService;
+    }
+
+    @PostMapping("products/{productId}/purchases")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Purchase postPurchase(@AuthenticationPrincipal UserDetails details,
+                                 @PathVariable Long productId, @RequestBody Purchase purchase) {
+        try {
+            return escudoService.addNewPurchase(details.getUsername(), productId, purchase);
+        } catch (ProductNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        } catch (IllegalAccessException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
+        }
+    }
+}
