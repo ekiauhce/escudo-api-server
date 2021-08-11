aba398a6a5a76fbdbf3db9595994c72cf740ab20
ekiauhce <ekiauhce@yandex.ru>
2021-08-11 16:24:32 +0300

    Cleanup imports

--- src/main/java/escudo/api/Application.java
+++ src/main/java/escudo/api/Application.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
