2ee31a57ad5f881446aca5832e784384a4b5cbc9
ekiauhce <ekiauhce@yandex.ru>
2021-08-14 21:36:21 +0300

    Add entity graph

--- src/main/java/escudo/api/repositories/ProductRepository.java
+++ src/main/java/escudo/api/repositories/ProductRepository.java
@@ -1,11 +1,14 @@
 package escudo.api.repositories;
 
 import escudo.api.entities.Product;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    @EntityGraph(attributePaths = "purchases")
     List<Product> findProductsByBuyerUsername(String username);
 }
