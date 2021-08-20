8af52559a5ce367262fa11ea498587aba1be2967
ekiauhce <ekiauhce@yandex.ru>
2021-08-20 19:24:41 +0300

    Refactoring: persist unix timestamp

--- src/main/java/escudo/api/entities/Purchase.java
+++ src/main/java/escudo/api/entities/Purchase.java
@@ -1,13 +1,14 @@
 package escudo.api.entities;
 
 
-import escudo.api.dtos.NewPurchaseDto;
+import escudo.api.dtos.PurchaseDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+
+import java.time.Instant;
 
 @Entity
 @Data
@@ -16,17 +17,17 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Purchase {
 
-    public Purchase(NewPurchaseDto dto) {
+    public Purchase(PurchaseDto dto) {
         this.price = dto.getPrice();
     }
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     private Double price;
 
-    private LocalDateTime madeAt;
+    private Long madeAt;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "product_id", referencedColumnName = "id")
@@ -34,6 +35,6 @@ public class Purchase {
 
     @PrePersist
     public void madeAt() {
-        madeAt = LocalDateTime.now();
+        madeAt = Instant.now().toEpochMilli();
     }
 }
--- src/main/java/escudo/api/services/EscudoService.java
+++ src/main/java/escudo/api/services/EscudoService.java
@@ -1,11 +1,22 @@
 package escudo.api.services;
 
-import escudo.api.dtos.NewProductDto;
-import escudo.api.dtos.NewPurchaseDto;
-import escudo.api.dtos.ProductListItemDto;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import escudo.api.dtos.ProductDto;
 import escudo.api.dtos.ProductPatchDto;
+import escudo.api.dtos.PurchaseCreationReportDto;
+import escudo.api.dtos.PurchaseDeletionReportDto;
+import escudo.api.dtos.PurchaseDto;
 import escudo.api.entities.Buyer;
 import escudo.api.entities.Product;
+import escudo.api.entities.ProductSummary;
 import escudo.api.entities.Purchase;
 import escudo.api.exceptions.DuplicateProductException;
 import escudo.api.exceptions.ProductNotFoundException;
@@ -13,21 +24,13 @@ import escudo.api.exceptions.PurchaseNotFoundException;
 import escudo.api.repositories.BuyerRepository;
 import escudo.api.repositories.ProductRepository;
 import escudo.api.repositories.PurchaseRepository;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Service
 @Transactional(rollbackFor = {
     DuplicateProductException.class,
     ProductNotFoundException.class,
-    PurchaseNotFoundException.class,
-    IllegalAccessException.class,
+    PurchaseNotFoundException.class
 })
 public class EscudoService {
 
@@ -41,14 +44,25 @@ public class EscudoService {
         this.purchaseRepo = purchaseRepo;
     }
 
-    public List<ProductListItemDto> getProducts(String username) {
+    public List<ProductDto> getProducts(String username) {
         return productRepo.findProductsByBuyerUsername(username).stream()
-            .map(ProductListItemDto::new)
+            .map(ProductDto::new)
             .collect(Collectors.toList());
     }
 
 
-    public NewProductDto addNewProduct(NewProductDto productDto, String username) throws DuplicateProductException {
+    public List<PurchaseDto> getPurchases(String username, String productName)
+        throws ProductNotFoundException {
+        
+        Product product = productRepo.findByBuyerUsernameAndName(username, productName)
+                .orElseThrow(() -> new ProductNotFoundException("No product with this name!"));
+
+        return product.getPurchases().stream()
+            .map(PurchaseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    public ProductDto addNewProduct(ProductDto productDto, String username) throws DuplicateProductException {
         Buyer buyer = buyerRepo.findByUsername(username);
         Product product = new Product(productDto);
         product.setBuyer(buyer);
@@ -59,53 +73,15 @@ public class EscudoService {
             throw new DuplicateProductException("Product with this name already exists!", e);
         }
 
-        return new NewProductDto(product);
+        return new ProductDto(product);
     }
 
-    public NewPurchaseDto addNewPurchase(String username, Long productId, NewPurchaseDto purchaseDto)
-        throws ProductNotFoundException, IllegalAccessException {
+    public void updateProduct(String username, String productName, ProductPatchDto productDto)
+        throws ProductNotFoundException, DuplicateProductException {
 
-        Product product = productRepo.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException("No product with this id!"));
-
-        if (!username.equals(product.getBuyer().getUsername())) {
-            throw new IllegalAccessException("You don't own this product!");
-        }
-
-        Purchase purchase = new Purchase(purchaseDto);
-        purchase.setProduct(product);
-        purchase = purchaseRepo.save(purchase);
-
-        return new NewPurchaseDto(purchase);
-    }
-
-
-    public void deletePurchase(String username, Long productId, Long purchaseId)
-        throws ProductNotFoundException, IllegalAccessException, PurchaseNotFoundException {
-
-        Product product = productRepo.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException("No product with this id!"));
-
-        if (!username.equals(product.getBuyer().getUsername())) {
-            throw new IllegalAccessException("You don't own this product!");
-        }
-
-        try {
-            purchaseRepo.deleteById(purchaseId);
-        } catch (EmptyResultDataAccessException e) {
-            throw new PurchaseNotFoundException("No purchase with this id!", e);
-        }
-    }
-
-    public void updateProduct(String username, Long productId, ProductPatchDto productDto)
-        throws ProductNotFoundException, IllegalAccessException, DuplicateProductException {
-
-        Product product = productRepo.findById(productId)
-                .orElseThrow(() -> new ProductNotFoundException("No product with this id!"));
-
-        if (!username.equals(product.getBuyer().getUsername())) {
-            throw new IllegalAccessException("You don't own this product!");
-        }
+        Product product = productRepo.findByBuyerUsernameAndName(username, productName)
+                .orElseThrow(() -> new ProductNotFoundException("No product with this name!"));
+        
         try {
             product.setName(productDto.getName());
         } catch (DataIntegrityViolationException e) {
@@ -113,16 +89,101 @@ public class EscudoService {
         }
     }
 
-    public void deleteProduct(String username, Long productId) 
-        throws ProductNotFoundException, IllegalAccessException {
-        
-        Product product = productRepo.findById(productId)
-            .orElseThrow(() -> new ProductNotFoundException("No product with this id"));
-
-        if (!username.equals(product.getBuyer().getUsername())) {
-            throw new IllegalAccessException("You don't own this product");
-        }
+    public void deleteProduct(String username, String productName) throws ProductNotFoundException {
+        Product product = productRepo.findByBuyerUsernameAndName(username, productName)
+            .orElseThrow(() -> new ProductNotFoundException("No product with this name!"));
 
         productRepo.delete(product);
+    }
+
+    public PurchaseCreationReportDto addNewPurchase(String username, String productName, PurchaseDto purchaseDto)
+        throws ProductNotFoundException {
+
+        Product product = productRepo.findByBuyerUsernameAndName(username, productName)
+                .orElseThrow(() -> new ProductNotFoundException("No product with this name!"));
+
+        Purchase purchase = new Purchase(purchaseDto);
+        purchase.setProduct(product);
+        purchase = purchaseRepo.save(purchase);
+
+        ProductSummary summary = new ProductSummary(avgCPD(product), avgLifespan(product));
+        product.setSummary(summary);
+
+        return new PurchaseCreationReportDto(purchase);
+    }
+
+
+    public PurchaseDeletionReportDto deletePurchase(String username, String productName, Long purchaseId)
+        throws ProductNotFoundException, PurchaseNotFoundException {
+
+        Product product = productRepo.findByBuyerUsernameAndName(username, productName)
+                .orElseThrow(() -> new ProductNotFoundException("No product with this name!"));
+
+        try {
+            purchaseRepo.deleteById(purchaseId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new PurchaseNotFoundException("No purchase with this id!", e);
+        }
+
+        ProductSummary summary = new ProductSummary(avgCPD(product), avgLifespan(product));
+        product.setSummary(summary);
+
+        return new PurchaseDeletionReportDto(product);
+    }
+
+    /**
+     * Calculates average cost per day over all pairs of adjacent purchases
+     * @param product
+     * @return
+     */
+    private double avgCPD(Product product) {
+        List<Purchase> purchases = product.getPurchases();
+        
+        if (purchases.size() < 2) {
+            return 0.0;
+        }
+
+        double[] costs = new double[purchases.size() - 1];
+        
+        Purchase prev, next;
+        for (int i = 0; i < purchases.size() - 1; i++) {
+            prev = purchases.get(i);
+            next = purchases.get(i + 1);
+            costs[i] = prev.getPrice() / lifespan(prev, next);
+        }
+
+        return DoubleStream.of(costs).average()
+            .orElse(0.0);
+    }
+
+    /**
+     * Calculates average lifespan over all pairs of adjacent purchases
+     * @param product
+     * @return
+     */
+    private double avgLifespan(Product product) {
+        List<Purchase> purchases = product.getPurchases();
+
+        if (purchases.size() < 2) {
+            return 0.0;
+        }
+
+        double[] lifespans = new double[purchases.size() -1];
+
+        Purchase prev, next;
+        for (int i = 0; i < purchases.size() - 1; i++) {
+            prev = purchases.get(i);
+            next = purchases.get(i + 1);
+            lifespans[i] = lifespan(prev, next);
+        }
+
+        return DoubleStream.of(lifespans).average()
+            .orElse(0.0);
+    }
+
+    // in days
+    private double lifespan(Purchase prev, Purchase next) {
+        double millis = next.getMadeAt() - prev.getMadeAt();
+        return  millis / (1000 * 60 * 60 * 24);
     }
 }
