f809d376bd2f68dcd609f5bbdb5dcf67bcba4989
ekiauhce <ekiauhce@yandex.ru>
2021-08-20 19:25:32 +0300

    Add find by username and product name method

--- src/main/java/escudo/api/repositories/ProductRepository.java
+++ src/main/java/escudo/api/repositories/ProductRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @EntityGraph(attributePaths = "purchases")
     List<Product> findProductsByBuyerUsername(String username);
+
+    Optional<Product> findByBuyerUsernameAndName(String username, String name);
 }
